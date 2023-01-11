e6922a8ca53b4835705f8668a9a92f74ddafedb3
Mira <mira@gmail.com>
2023-01-11 12:23:45 +0200

    Dark&Light

---
+++ javaScript-AppleSeeds/Dark-light.js
@@ -0,0 +1,29 @@
+function Dark_Light(color){
+switch (color){
+case "pink":
+    console.log("light color");
+    break;
+case "orange":
+    console.log("light color");
+    break;
+case "yellow":
+    console.log("light color");
+    break;
+case "blue":
+    console.log("dark color");
+    break;
+case "brown":
+    console.log("sark color");
+    break;  
+case "purple":
+    console.log("dark color");        
+    break;
+default:
+    console.log("Unknown color");
+}
+}
+Dark_Light("orange");
+Dark_Light("pink");
+Dark_Light("blue");
+Dark_Light("brown");
+Dark_Light("black");
