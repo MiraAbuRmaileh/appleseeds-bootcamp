0d60c2d7a7fa254b2c901abce8c22a9381555f3c
Mira <mira@gmail.com>
2023-01-11 15:35:47 +0200

    book

---
+++ javaScript-AppleSeeds/book.js
@@ -0,0 +1,10 @@
+const book = {
+bookname:"javaScript",
+author_name:"John",
+author_Age:56,
+publishing_year:1990
+};
+function detail(book){
+console.log(`The book ${book.bookname} was written by ${book.author_name} he is ${book.author_Age} old in ${book.publishing_year}`);
+}
+detail(book);
