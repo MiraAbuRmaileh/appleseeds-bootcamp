ddb7d34223e4b7984d4ffd7da4aefda10d52abdd
Mira <mira@gmail.com>
2023-01-15 15:53:18 +0200

    string length by using a while loop

---
+++ javaScript-AppleSeeds/loopsAndWhile.js
@@ -0,0 +1,10 @@
+function stringLengths2(string1){
+    let j =0;
+    while( j <=0){
+        console.log(words);
+        j++;
+    }
+}
+const words = ["boo", "doooo", "hoo","ro"];
+const print_words = stringLengths2(words);
+console.log(print_words);
