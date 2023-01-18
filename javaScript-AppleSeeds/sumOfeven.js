5391d2cc78d8d692d20cc1f328f3e142736adc81
Mira <mira@gmail.com>
2023-01-18 11:31:49 +0200

    debugging of sum of evens by indexes

---
+++ javaScript-AppleSeeds/sumOfeven.js
@@ -0,0 +1,14 @@
+// 1
+// the error in return
+//2 
+// debugging in my eyes .
+// 3
+// the error that you arr think that the indexes un the array start with 1 as 
+// the sum evens in the indexes way gonna be wrong .
+// 4 
+function getSumOfEven(arr){
+    // error in the indexes when you did the sum .
+    return arr[1] + arr[3] + arr[5] + arr[7] +
+    arr[9];
+    }
+   console.log(getSumOfEven([1,2,3,4,5,6,7,8,9,10]));
