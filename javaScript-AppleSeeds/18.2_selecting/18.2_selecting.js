f04e1b3b3b42c9549a246c399e82fc80c324451f
Mira <mira@gmail.com>
2023-01-30 10:35:45 +0200

    selcting elemints by using a html code

---
+++ javaScript-AppleSeeds/18.2_selecting/18.2_selecting.js
@@ -0,0 +1,10 @@
+// 1 
+document.querySelector('h1').style.color = 'blue';
+// 2 
+document.querySelector('.paragraph').style.fontSize = '30px';
+// 3 
+document.querySelector('#description').style.display = 'inline-block';
+// 4 
+document.body.style.backgroundColor = 'green';
+// 5 
+document.title = 'New Title';
